caebda27a820e3281889b0651596814cdbb00c93
Julia Foresti <juliajforesti@gmail.com>
2021-03-16 11:02:25 -0300

    run build to fix deploy

---
+++ public/precache-manifest.ce16ae715c169e940727c26204feff8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30515c96784dd1403448e1ce1bf17dea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cd254c1b26b1414c0a83",
+    "url": "./static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "2f2b51a79712699074ff",
+    "url": "./static/css/main.aeabb5d5.chunk.css"
+  },
+  {
+    "revision": "cd254c1b26b1414c0a83",
+    "url": "./static/js/2.fea444c4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.fea444c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2b51a79712699074ff",
+    "url": "./static/js/main.8c2a1552.chunk.js"
+  },
+  {
+    "revision": "7d5aa159024a79ebb6f2",
+    "url": "./static/js/runtime-main.02370d18.js"
+  },
+  {
+    "revision": "0cd0ca797ca5aa4e99eed6e8f40f4830",
+    "url": "./static/media/hxagon.0cd0ca79.png"
+  },
+  {
+    "revision": "8cd4ce7c3958403ebf8533811cec97da",
+    "url": "./static/media/project-flow.8cd4ce7c.png"
+  }
+]);
